8919f58bfb3964c9582681a1c61367b890c3ebc1
basilcea <ogbonna.basil3@gmmail.com>
2019-06-24 10:29:34 +0100

    Add routes to page in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import LandingPage from './components/container'
+import LoginPage from './components/login'
+import AddJournalPage from './components/journalForm'
+import AddRepsPage from './components/repForm'
 
 const App = () =>{
     return(
         <div>
-        
+        <Route exact path ='/' component={LandingPage}/>
+        <Route path='/login' component= {LoginPage} />
+        <Route path='/add' component = {AddJournalPage} />
+        <Route path='/reps/add' component = {AddRepsPage} />
+        <Route path='/update/:id' component = {AddJournalPage} />
+        <Route path = '/reps/update/:id' component = {AddRepsPage} />
         </div>
     )
 }
